9b7e8c0e9b84f06fc1c909da5b53699681bcaa64
Greg Kempe <greg@kempe.net>
2016-09-23 11:57:05 +0200

    Adjust refit.py

--- refit.py
+++ refit.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from shapely.geometry import shape
 from shapely.geos import TopologicalError
 from fiona import collection
@@ -6,24 +7,29 @@ import rtree
 
 LAYER1 = "data/sal.shp"
 LAYER1_KEY = "sal_code_i"
-LAYER2 = "/Users/greg/coding/SA-Maps/Wards/Wards2011.shp"
-LAYER2_KEY = "WARD_ID"
+LAYER2 = "data/Police_bounds.shp"
+LAYER2_KEY = "COMPNT_NM"
 
-with open("output_remap.csv", "w") as fp:
+with open("sal_precinct_weights.csv", "w") as fp:
     layer1 = collection(LAYER1, "r")
     layer2 = collection(LAYER2, "r")
 
-    fp.write("sal_code,ward_code,weight\n")
-    index = rtree.index.Index()
-    sys.stderr.write("Creating index over %d items\n" % len(layer1))
+    fp.write("sal_code,precinct,weight\n")
 
-    for feat1 in layer1:
-        fid = int(feat1['id'])
-        geom1 = shape(feat1['geometry'])
-        index.insert(fid, geom1.bounds)
-        sys.stderr.write(".")
-    sys.stderr.write("Index created")
+    if os.path.exists('sal.idx'):
+        index = rtree.index.Index('sal')
+        print "Using existing index"
+    else:
+        index = rtree.index.Index('sal')
+        print "Creating index over %d items\n" % len(layer1)
 
+        for feat1 in layer1:
+            fid = int(feat1['id'])
+            geom1 = shape(feat1['geometry'])
+            index.insert(fid, geom1.bounds)
+        print "Index created"
+
+    sys.stderr.write("Intersecting over %d items\n" % len(layer2))
     for feat2 in layer2:
         geom2 = shape(feat2['geometry'])
         for fid in list(index.intersection(geom2.bounds)):
@@ -33,7 +39,7 @@ with open("output_remap.csv", "w") as fp:
                 intersection = geom1.intersection(geom2)
                 if intersection.area > 0:
                     feat1_key = int(feat1["properties"][LAYER1_KEY])
-                    feat2_key = int(feat2["properties"][LAYER2_KEY])
+                    feat2_key = feat2["properties"][LAYER2_KEY]
                     fp.write("%s,%s,%s\n" % (feat1_key, feat2_key, intersection.area / geom1.area))
             except TopologicalError, e:
                 print e
